38a6cddea12542716d44b4ad6ea644e59eace774
nilbernilay-hash <nilbernilay@gmail.com>
2026-07-08 18:10:39 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,9 @@ import 'tldraw/tldraw.css'
 function App() {
 	return (
 		<div style={{ position: 'fixed', inset: 0 }}>
-			<Tldraw />
+			<Tldraw
+				licenseKey="tldraw-2026-10-16/WyJnVWNsYS03MCIsWyIqIl0sMTYsIjIwMjYtMTAtMTYiXQ.8Jf1JNtQxadcXBJ2i7xA6NOvPxzi5L3JQ8IQxwEvuPJpC/zzQu8QPbNfFG3KbiLVdD4W/ZDP5rxW/NEWbbFXiA"
+			/>
 		</div>
 	)
 }
